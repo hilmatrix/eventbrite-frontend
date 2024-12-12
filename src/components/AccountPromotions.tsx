f878e342257b269bcbaa15cfae58d0f1a040e4aa
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2024-12-12 19:39:58 +0700

    added delete promotion feature

--- src/components/AccountPromotions.tsx
+++ src/components/AccountPromotions.tsx
@@ -1,12 +1,14 @@
-import { API_ORGANIZER_PROMOTIONS } from "@/constants/api";
+import { API_ORGANIZER_PROMOTIONS, API_PROMOTIONS } from "@/constants/api";
 import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from 'next/navigation'; // Import useRouter from next/navigation
 import { useEffect, useState } from "react";
 
 export default function PromotionsList() {
   const { getJwtToken } = useAuth();
   const [promotions, setPromotions] = useState([]);
   const [promotionsLoaded, setPromotionsLoaded] = useState(false);
-
+  const router = useRouter(); // Initialize router
+  
   useEffect(() => {
     const fetchPromotions = async () => {
       try {
@@ -32,6 +34,33 @@ export default function PromotionsList() {
     fetchPromotions();
   }, []);
 
+  const handleDelete = async (promoId) => {
+    const result = confirm("Are you sure you want to delete this promotion?");
+    if (result) 
+      await deletePromotion(promoId);
+  }
+  
+  const deletePromotion = async (promoId) => {
+    try {
+      const token = getJwtToken();
+      const response = await fetch(`${API_PROMOTIONS}/${promoId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      if (response.ok) {
+        alert("Successfully removed promotion");
+        router.refresh(); // Refreshes the page using next/navigation
+      } else {
+        alert(`Failed to remove promotion (status: ${response.status})`);
+      }
+    } catch (error) {
+      alert("Failed to remove promotion: " + error);
+    }
+  }
+
   return (
     <>
       <div className="bg-white shadow-md rounded-lg p-6 my-2 h-[50%] w-[90%] md:w-[80%]">
@@ -49,6 +78,14 @@ export default function PromotionsList() {
                   <p><strong>Created At:</strong> {new Date(promotion.createdAt).toLocaleString()}</p>
                   <p><strong>Updated At:</strong> {new Date(promotion.updatedAt).toLocaleString()}</p>
                   <p><strong>Deleted At:</strong> {promotion.deletedAt ? new Date(promotion.deletedAt).toLocaleString() : "N/A"}</p>
+                  <div className="m-2 flex justify-center">
+                    <button 
+                      onClick={() => handleDelete(promotion.promoId)} 
+                      className="rounded-[10px] bg-[#FF0000] h-10 p-2 flex items-center justify-center"
+                    >
+                      Delete Promotion
+                    </button>
+                  </div>
                 </li>
               ))}
             </ul>
